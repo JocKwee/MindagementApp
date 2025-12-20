a487d7d9afb36f6490c4ec3a9b0560cb8dcb44d1
JocKwee <jocelyne.kwee27@ycdsbk12.ca>
2025-12-20 10:44:40 -0500

    Edited Doctor.java

--- .src/model/Doctor.java
+++ .src/model/Doctor.java
@@ -1,5 +1,13 @@
 package model;
 
 public class Doctor {
-    
+    public String name;
+    public String specialty;
+    public String contactInfo;
+
+    public Doctor(String name, String specialty, String contactInfo) {
+        this.name = name;
+        this.specialty = specialty;
+        this.contactInfo = contactInfo;
+    }
 }
