cb9778a204f7610609810946266605224ccb84a8
JocKwee <jocelyne.kwee27@ycdsbk12.ca>
2025-12-20 10:46:23 -0500

    Edited Medication.java

--- .src/model/Medication.java
+++ .src/model/Medication.java
@@ -1,5 +1,16 @@
 package model;
 
 public class Medication {
-    
+    public String name;
+    public String type;
+    public String dosage;
+    public String frequency;
+    public String prescribingDoctor;
+    public Medication(String name, String type, String dosage, String frequency, String prescribingDoctor) {
+        this.name = name;
+        this.type = type;
+        this.dosage = dosage;
+        this.frequency = frequency;
+        this.prescribingDoctor = prescribingDoctor;
+    }
 }
