b8de598512ac3fe1e22bbcc27713f3f8d87a1c3f
JocKwee <jocelyne.kwee27@ycdsbk12.ca>
2025-12-20 10:47:13 -0500

    Edited Reminder.java

--- .src/model/Reminder.java
+++ .src/model/Reminder.java
@@ -1,5 +1,10 @@
 package model;
 
 public class Reminder {
-    
+    public String message;
+    public String time;
+    public Reminder(String message, String time) {
+        this.message = message;
+        this.time = time;
+    } // idk
 }
