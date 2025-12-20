c392e0dcb8c3b37d95d0bec11266c83f438ed121
JocKwee <jocelyne.kwee27@ycdsbk12.ca>
2025-12-20 10:48:03 -0500

    Edited CalanderEvent.java

--- .src/model/CalendarEvent.java
+++ .src/model/CalendarEvent.java
@@ -1,5 +1,15 @@
 package model;
 
 public class CalendarEvent {
-    
+    public String title;
+    public String date;
+    public String time;
+    public String description;
+
+    public CalendarEvent(String title, String date, String time, String description) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+        this.description = description;
+    }
 }
