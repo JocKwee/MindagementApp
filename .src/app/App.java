d12b6ef153ee195279bfb92db660873e1dc6d223
JocKwee <jocelyne.kwee27@ycdsbk12.ca>
2025-12-22 15:31:31 -0500

    Moved app file into .scr

---
+++ .src/app/App.java
@@ -0,0 +1,20 @@
+package app;
+
+import auth.LoginOrRegisterGUI;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            LoginOrRegisterGUI loginGui = new LoginOrRegisterGUI();
+            loginGui.setVisible(true);
+        });
+    }
+}
